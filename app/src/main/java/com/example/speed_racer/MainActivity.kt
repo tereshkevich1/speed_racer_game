c55f3cc3f17200a5cbd669bf14df016a3aa19ef8
tereshkevich1 <tereshkevich.cergey@mail.ru>
2025-02-25 19:50:19 +0300

    refactor: move GameApp composable to presentation layer

--- app/src/main/java/com/example/speed_racer/MainActivity.kt
+++ app/src/main/java/com/example/speed_racer/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.example.speed_racer.presentation.navigation.AppNavGraph
-import com.example.speed_racer.ui.theme.Speed_racerTheme
+import com.example.speed_racer.presentation.GameApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,27 +16,6 @@ class MainActivity : ComponentActivity() {
             GameApp()
         }
     }
-}
-
-@Composable
-fun GameApp() {
-    val navController = rememberNavController()
-    Speed_racerTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            AppNavGraph(
-                navHostController = navController,
-                modifier = Modifier.padding(innerPadding)
-            )
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 
--- app/src/main/java/com/example/speed_racer/presentation/start_screen/StartScreen.kt
+++ app/src/main/java/com/example/speed_racer/presentation/start_screen/StartScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
